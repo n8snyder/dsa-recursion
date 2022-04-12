2669379da72fe2638c907af19b9138801155660a
Nate Snyder <n8.snyder@berkeley.edu>
2022-04-12 11:59:13 -0700

    completed product, longest, everyOther, isPalidrome

--- recursion.js
+++ recursion.js
@@ -1,25 +1,36 @@
 /** product: calculate the product of an array of numbers. */
 
 function product(nums) {
-
+  if (nums.length === 0) return 1;
+  return nums[0] * product(nums.splice(1));
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-
+  if (words.length === 0) return 0;
+  const wordLength = words[0].length;
+  const longestOfOtherWords = longest(words.splice(1));
+  if (wordLength > longestOfOtherWords) return wordLength;
+  else return longestOfOtherWords;
 }
 
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str) {
+  if (str.length === 0) return "";
 
+  return str[0] + everyOther(str.slice(2));
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
-
+  if (str.length === 1 || str.length === 0) return true;
+  const first = str[0];
+  const last = str[str.length - 1];
+  if (first !== last) return false;
+  return isPalindrome(str.slice(1, str.length - 1));
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */

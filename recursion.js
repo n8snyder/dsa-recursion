27c1cecda193f278168cd0190a8d516d9e7532ec
Nate Snyder <n8.snyder@berkeley.edu>
2022-04-12 16:39:29 -0700

    add faster versions of our functions

--- recursion.js
+++ recursion.js
@@ -5,12 +5,25 @@ function product(nums) {
   return nums[0] * product(nums.splice(1));
 }
 
+function otherProduct(nums, i = 0) {
+  if (i === nums.length) return 1;
+  return nums[i] * otherProduct(nums, i + 1);
+}
+
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
   if (words.length === 0) return 0;
   const wordLength = words[0].length;
   const longestOfOtherWords = longest(words.splice(1));
+  if (wordLength > longestOfOtherWords) return wordLength;
+  else return longestOfOtherWords;
+}
+
+function otherLongest(words, i = 0) {
+  if (i === words.length) return 0;
+  const wordLength = words[i].length;
+  const longestOfOtherWords = otherLongest(words, i + 1);
   if (wordLength > longestOfOtherWords) return wordLength;
   else return longestOfOtherWords;
 }
@@ -23,6 +36,12 @@ function everyOther(str) {
   return str[0] + everyOther(str.slice(2));
 }
 
+function otherEveryOther(str, i = 0) {
+  if (i >= str.length) return "";
+
+  return str[i] + otherEveryOther(str, i + 2);
+}
+
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
@@ -33,10 +52,19 @@ function isPalindrome(str) {
   return isPalindrome(str.slice(1, str.length - 1));
 }
 
+function otherIsPalindrome(str, i = 0) {
+  const first = i;
+  const last = str.length - 1 - i;
+
+  if (first >= last) return true;
+
+  if (str[first] !== str[last]) return false;
+  return otherIsPalindrome(str, i + 1);
+}
+
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val) {
-  console.log(arr, val);
   if(arr.length === 0) return -1;
   if(arr[0]===val) return 0;
   const findResult = findIndex(arr.slice(1), val);
@@ -47,8 +75,14 @@ function findIndex(arr, val) {
   else{
     return 1 + findResult;
   }
-  
 }
+
+function otherFindIndex(arr, val, i = 0) {
+  if(i === arr.length) return -1;
+  if(arr[i] === val) return i;
+  return otherFindIndex(arr, val, i + 1);
+}
+
 
 /** revString: return a copy of a string, but in reverse. */
 
@@ -56,6 +90,12 @@ function revString(str) {
   if (str.length === 0) return "";
 
   return revString(str.slice(1)) + str[0];
+}
+
+function otherRevString(str, i = 0) {
+  if (i === str.length) return "";
+
+  return otherRevString(str, i + 1) + str[i];
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
@@ -70,6 +110,13 @@ function gatherStrings(obj) {
     if (typeof obj[keys[0]] === "object") strings.push(...gatherStrings(obj[keys[0]]));
     _gatherStrings(keys.slice(1));
   }
+
+  function _otherGatherStrings(keys, i = 0){
+    if (i === keys.length) return;
+    if (typeof obj[keys[i]] === "string") strings.push(obj[keys[i]]);
+    if (typeof obj[keys[i]] === "object") strings.push(...gatherStrings(obj[keys[i]]));
+    _gatherStrings(keys, i + 1);
+  }
   
   _gatherStrings(Object.keys(obj));
   return strings;
@@ -82,18 +129,14 @@ function gatherStrings(obj) {
 function binarySearch(arr, val) {
 
   function _binarySearch(arr, val, left, right){
-    
     let middle = Math.floor((left + right)/2);
     if(arr[middle] === val) return middle;
     else if (left === right) return -1;
     if ( arr[middle] > val) return _binarySearch(arr, val, left, middle-1);
     else if ( arr[middle] < val) return _binarySearch(arr, val, middle+1, right);
-
   }
 
   return _binarySearch(arr, val, 0, arr.length-1);
-  
-
 }
 
 module.exports = {
